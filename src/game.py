2709b47b27adff087ca0642fb9da97dc5a732446
AhmedBasem <demhamesab@gmail.com>
2023-02-01 03:19:08 +0200

    display pieces on the board

--- src/game.py
+++ src/game.py
@@ -1,10 +1,11 @@
 import pygame
+from board import *
 from const import *
 
 class Game:
 
     def __init__(self):
-        pass
+        self.board = Board()
     
     def show_background(self, surface):
         """Draw the chess background
@@ -12,11 +13,19 @@ class Game:
         for row in range(ROWS):
             for col in range(COLS):
                 if (row + col) % 2 == 0:
-                    color = (255, 255, 255)
+                    color = (200, 200, 200)
                 else:
-                    color = (0, 0, 0)
+                    color = (100, 50, 30)
                     
                 rect = (col * SQUARE_SIZE, row * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE)
                 pygame.draw.rect(surface, color, rect)
-    def show_pieces():
-        pass
+    def show_pieces(self, surface):
+        for row in range(ROWS):
+            for col in range(COLS):
+                if self.board.squares[row][col].has_piece():
+                    piece = self.board.squares[row][col].piece
+                    
+                    img = pygame.image.load(piece.texture)
+                    img_center = col * SQUARE_SIZE + SQUARE_SIZE // 2, row * SQUARE_SIZE + SQUARE_SIZE // 2
+                    piece.texture_rect = img.get_rect(center=img_center)
+                    surface.blit(img, piece.texture_rect)
